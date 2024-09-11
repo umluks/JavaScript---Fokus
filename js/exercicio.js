b5b1757890e5878be01c559ae6b1b6a06ced3aca
Lucas Santiago <lucas.santiago@eb.mil.br>
2024-09-11 16:23:30 -0300

    arquivos

---
+++ js/exercicio.js
@@ -0,0 +1,11 @@
+const html = document.querySelector("html");
+console.log(html);
+
+const timer = document.querySelector(".app__card-timer");
+console.log(timer);
+
+const appImg = document.querySelector(".app__image");
+console.log(appImg);
+
+const appTitle = document.querySelector(".app__title");
+console.log(appTitle);
